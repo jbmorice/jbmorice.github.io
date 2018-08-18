a6cd6dfbc57d513e0a5bb5f527dd64a970fa2ef6
Jean-Baptiste Morice <jeanbaptiste.morice@gmail.com>
2018-08-18 13:35:48 +0200

    Finished skills animations

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -3,7 +3,7 @@
 	function SlidingSkills( element ) {
 		this.element = element;
 		this.skills = this.element.getElementsByClassName("js-skill");
-		this.offset = 0.8;
+		this.offset = 0.9;
 		this.hideBlocks();
 	};
 
@@ -23,18 +23,18 @@
 	};
 
 	SlidingSkills.prototype.showBlocks = function() {
-		// if ( ! "classList" in document.documentElement ) {
-		// 	return;
-		// }
-		// var self = this;
-		// for( var i = 0; i < this.skills.length; i++) {
-		// 	(function(i){
-		// 		if( self.skills[i].classList.contains("cd-is-hidden") && self.element.getBoundingClientRect().top <= window.innerHeight*self.offset ) {
-		// 			self.skills[i].classList.add("resume-skill-badge--slide-in");
-		// 			self.skills[i].classList.remove("cd-is-hidden");
-		// 		}
-		// 	})(i);
-		// }
+		if ( ! "classList" in document.documentElement ) {
+			return;
+		}
+		var self = this;
+		for( var i = 0; i < this.skills.length; i++) {
+			(function(i){
+				if( self.skills[i].classList.contains("cd-is-hidden") && self.element.getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+					self.skills[i].classList.remove("cd-is-hidden");
+					self.skills[i].classList.add("resume-skill-badge--slide-in");
+				}
+			})(i);
+		}
 	};
 
 	var slidingSkillsBlocks = document.getElementsByClassName("js-skill-block"),
